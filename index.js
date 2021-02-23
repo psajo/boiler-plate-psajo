03ab2d0500ccbde150f12135aa0f20e957c8914b
psajogg <psajogg@gmail.com>
2021-02-23 16:59:20 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+const { User } = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sangjong:1234@cluster0.yw0v6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+app.get('/', (req, res) => res.send('Hello World! '))
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!123')
+app.post('/register',(req,res)=> {
+    const user =new User(req.body)
+    user.save((err,doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
